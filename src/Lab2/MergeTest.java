fc8361b8905d97df5fa2c3edb739bebdd2c69b79
Gagernaus <56046874+Gagernaus@users.noreply.github.com>
2021-11-01 13:53:19 +0300

    Add files via upload

--- src/Lab2/MergeTest.java
+++ src/Lab2/MergeTest.java
@@ -5,11 +5,15 @@ import Lab2.LList;
 public class MergeTest {
     public static void main(String[] args) {
         LList a = new LList();
-        for (int i = 0; i < 4; ++i) {
-            a.add(i);
-        }
+//        for (int i = 4; i < 10; ++i) {
+//            a.add(i);
+//        }
+        a.add(-10);
+        a.add(3);
+        a.add(5);
+        a.add(34);
         LList b = new LList();
-        for (int i = 4; i < 10; ++i) {
+        for (int i = -3; i < 2; ++i) {
             b.add(i);
         }
         System.out.println(a.toString());
